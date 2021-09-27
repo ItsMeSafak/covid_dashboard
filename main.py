2517cd50c4e18d56bf892849ba25de1db996ddd6
Safak Inan <safak.inan61@hotmail.com>
2021-09-27 21:25:31 +0200

    added plots, stil needs sliders and descriptions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import inspect
 
-import components.Noah_plotly as figg
+import components.Noah_plotly as noh_figg
+import components.sfk_plots as sfk_figg
+
 import components.base as base
 
 def show_with_options(header, func):
@@ -10,10 +12,17 @@ def show_with_options(header, func):
     if base.showCode: st.code(inspect.getsource(func))
 
 def initialize_plots():
-    show_with_options('Landelijke IC opnames', figg.ic)
-    show_with_options('Afgenomen testen', figg.testen)
-    show_with_options('Rioolwater data', figg.riool)
-    show_with_options('Ziekenhuis opnames en dodental', figg.Opname_overlijden)
+    col1, col2, col3 = st.columns([4, 1, 4])
+
+    with col1:
+        show_with_options('Landelijke IC opnames', noh_figg.ic)
+        show_with_options('Afgenomen testen', noh_figg.testen)
+        show_with_options('Leeftijds groepen', sfk_figg.age_groups)
+
+    with col3:
+        show_with_options('Rioolwater data', noh_figg.riool)
+        show_with_options('Ziekenhuis opnames en dodental', noh_figg.Opname_overlijden)
+
 if __name__ == "__main__":
     base.main()
     initialize_plots()
