ff4af48bfa90a7f5a7f657d9c6eee89e469ba9ab
Safak Inan <safak.inan61@hotmail.com>
2021-09-29 22:32:25 +0200

    added text

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import components.sfk_plots as sfk_figg
 import components.Fleur as fleur
 import components.base as base
 
-def show_with_options(header, func):
+def show_with_options(header, func, text=''):
     st.header(header)
+    if (text != ''):
+        st.markdown('*' + text + '*')
     with st.expander(header, True):
         if base.showPlots: func()
         if base.showCode: st.code(inspect.getsource(func))
@@ -41,32 +43,34 @@ def initialize_plots():
     col4, _, col6 = st.columns([4, 1, 4])
     col7, _, col9 = st.columns([4, 1, 4])
 
-    with header_col:
-        show_with_options('Landelijke IC opnames', noh_figg.ic)
+    with open('assets/toelichting.txt') as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+        with header_col:
+            show_with_options('Landelijke IC opnames', noh_figg.ic, lines[0])
 
-    with col1:
-        st.markdown('*Sectie 1*')
-        show_with_options('Dodental per geslacht', noh_figg.sex_dec)
+        with col1:
+            st.markdown('*Sectie 1*')
+            show_with_options('Dodental per geslacht', noh_figg.sex_dec, lines[1])
+        with col3:
+            st.markdown('  ')
+            st.markdown('  ')
+            show_with_options('Reproductie getal', fleur.reproG, lines[2])
+            show_with_options('Leeftijdsgroepen', sfk_figg.age_groups)
 
-    with col3:
-        st.markdown('  ')
-        st.markdown('  ')
-        show_with_options('Reproductie getal', fleur.reproG)
-        show_with_options('Leeftijdsgroepen', sfk_figg.age_groups)
-    
-    with col4:
-        st.markdown('*Sectie 2*')
-        show_with_options('Afgenomen testen', noh_figg.testen2)
-    with col6:
-        st.markdown(' ')
-        show_with_options('Ziekenhuisopnames en dodental', noh_figg.Opname_overlijden2)
+        with col4:
+            st.markdown('*Sectie 2*')
+            show_with_options('Afgenomen testen', noh_figg.testen2, lines[3] + '\n' + lines[4])
+        with col6:
+            st.markdown(' ')
+            show_with_options('Ziekenhuisopnames en dodental', noh_figg.Opname_overlijden2, lines[5])
 
-    with col7:
-        st.markdown('*Sectie 3*')
-        show_with_options('Ziekenhuisopnames in leeftijdsgroepen', sfk_figg.admissions)
-    with col9:
-        st.write('  ')
-        show_with_options('Rioolwater data', noh_figg.riool)
+        with col7:
+            st.markdown('*Sectie 3*')
+            show_with_options('Ziekenhuisopnames in leeftijdsgroepen', sfk_figg.admissions, lines[6])
+        with col9:
+            st.write('  ')
+            show_with_options('Rioolwater data', noh_figg.riool, lines[7])
 
 if __name__ == "__main__":
     base.main()
