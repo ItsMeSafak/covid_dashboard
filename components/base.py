ea447406b8128c257beea128b016cbcbc22f34cb
Safak Inan <safak.inan61@hotmail.com>
2021-09-30 18:12:06 +0200

    quick link to documenation

--- components/base.py
+++ components/base.py
@@ -52,6 +52,7 @@ def sidebar():
                                             'Zuid-Holland-Zuid', 'Hollands-Midden', 'Rotterdam-Rijnmond', 'Haaglanden',
                                             'Zeeland', 'Brabant-Zuidoost', 'Midden- en West-Brabant', 'Brabant-Noord',
                                             'Limburg-Zuid', 'Limburg-Noord']))
+    st.sidebar.markdown('*Documentation: https://github.com/ItsMeSafak/covid_dashboard/blob/master/README.md*')
 
 def set_bg():
     if showBackground:
